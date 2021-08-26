3b2487944b75d11deea9fd8821068d922022eecb
Jalon Duncan <duncanjalon@gmail.com>
2021-08-26 11:48:02 -0500

    starting phase

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace GussingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to play a game?");
+            Console.ReadLine();
+
+            var ratings = new List<int>
+            {
+
+            };
+
+            Console.WriteLine("Choose your fun!");
+            Console.WriteLine("-----------------");
+        }
+    }
+}
