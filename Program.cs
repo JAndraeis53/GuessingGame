75ed41c1685ae57b7a5af98a883d1b7d4c68ec19
Jalon Duncan <duncanjalon@gmail.com>
2021-08-26 15:07:40 -0500

    moving on from part 1

--- Program.cs
+++ Program.cs
@@ -7,16 +7,34 @@ namespace GussingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you want to play a game?");
-            Console.ReadLine();
 
-            var ratings = new List<int>
+// Display a message to the user asking them to guess the secret number.
+            Console.WriteLine("Dare to guess my secret number");
+// Display a prompt for the user's guess.
+            Console.Write("Enter a number if you dare ");
+// Take the user's guess as input and save it as a variable.
+            string response = Console.ReadLine();
+// Display the user's guess back to the screen.
+            Console.WriteLine(response);
+
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+            int secretNumber = 42; 
+// No longer display the user's guess. They know what they guessed, right?
+            int guess = int.Parse(response);
+// Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
+            if (secretNumber == guess)
             {
+                Console.WriteLine("...Congratulations...");
+            }
+            else 
+            {
+                Console.WriteLine("FAILURE!");
+            }
 
-            };
+// Give the user four chances to guess the number.
 
-            Console.WriteLine("Choose your fun!");
-            Console.WriteLine("-----------------");
+// Continue to display the success or failure messages as in phase 2
+
         }
     }
 }
